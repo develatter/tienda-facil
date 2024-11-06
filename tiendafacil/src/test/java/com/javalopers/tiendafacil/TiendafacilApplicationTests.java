8f96e0acd247bed5403c52ea470c1b89b045db87
Lattecast <rattack.hyde@gmail.com>
2024-11-06 22:34:13 +0100

    creación del proyecto springboot

---
+++ tiendafacil/src/test/java/com/javalopers/tiendafacil/TiendafacilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javalopers.tiendafacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendafacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
