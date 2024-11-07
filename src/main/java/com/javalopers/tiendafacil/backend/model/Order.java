db0066372c4cf1684623f7a8cce67c0bbe868180
Lattecast <rattack.hyde@gmail.com>
2024-11-08 00:25:21 +0100

    Se implementan la entidad de Producto y el CRUD básico de los pedidos.

--- src/main/java/com/javalopers/tiendafacil/backend/model/Order.java
+++ src/main/java/com/javalopers/tiendafacil/backend/model/Order.java
@@ -1,4 +1,30 @@
 package com.javalopers.tiendafacil.backend.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "orders")
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_id")
+    private Integer orderId;
+
+    @Column(name = "customer_id", nullable = false)
+    private Integer customerId;
+
+    @Column(name = "order_date", nullable = false)
+    private LocalDateTime orderDate;
+
+    @Column(name = "delivery_date", nullable = false)
+    private LocalDate deliveryDate;
+
+    @Column(name = "status_id", nullable = false)
+    private Integer statusId;
 }
--- src/main/java/com/javalopers/tiendafacil/backend/service/OrderServiceImpl.java
+++ src/main/java/com/javalopers/tiendafacil/backend/service/OrderServiceImpl.java
@@ -1,4 +1,41 @@
 package com.javalopers.tiendafacil.backend.service;
 
-public class OrderServiceImpl {
+import com.javalopers.tiendafacil.backend.model.Order;
+import com.javalopers.tiendafacil.backend.repository.OrderRepository;
+import com.javalopers.tiendafacil.backend.service.interfaces.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+
+    private final OrderRepository orderRepository;
+
+    @Autowired
+    public OrderServiceImpl(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    @Override
+    public Order saveOrder(Order order) {
+        return orderRepository.save(order);
+    }
+
+    @Override
+    public Optional<Order> findOrderById(Integer orderId) {
+        return orderRepository.findById(orderId);
+    }
+
+    @Override
+    public void deleteOrderById(Integer orderId) {
+        orderRepository.deleteById(orderId);
+    }
+
+    @Override
+    public Collection<Order> getAllOrders() {
+        return orderRepository.findAll();
+    }
 }
