99ca915bc06981551b407fa14bac08f8ca54cf26
DidierSML <didiersmaldonad@gmail.com>
2024-11-09 08:45:59 -0500

    Implementación y conexión entre la Entidad "Customer" su respectivo Servicio, Controlador, Repositorio y su Clase DTO.

--- src/main/java/com/javalopers/tiendafacil/backend/model/Customer.java
+++ src/main/java/com/javalopers/tiendafacil/backend/model/Customer.java
@@ -1,10 +1,42 @@
 package com.javalopers.tiendafacil.backend.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
+@Table(name = "customers")
+@Data
 public class Customer {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer customerId;
+
+    @Column(name = "first_name", nullable = false, length = 100)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 300)
+    private String lastName;
+
+    @Column(nullable = false, length = 100)
+    private String address;
+
+    @Column(nullable = false, length = 100)
+    @Email
+    private String mail;
+
+    @Column(name = "reg_date", nullable = false, updatable = false)
+    private LocalDateTime regDate;
+
+    @Column(nullable = false)
+    private Boolean active;
+
+    // Relación con Order
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+    private List<Order> orders;
 }
+
--- src/main/java/com/javalopers/tiendafacil/backend/repository/CustomerRepository.java
+++ src/main/java/com/javalopers/tiendafacil/backend/repository/CustomerRepository.java
@@ -1,4 +1,11 @@
 package com.javalopers.tiendafacil.backend.repository;
 
-public interface CustomerRepository {
+import com.javalopers.tiendafacil.backend.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository <Customer,Integer> {
+
+    boolean existsByMail (String mail);
 }
