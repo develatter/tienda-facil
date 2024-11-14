1ead7b92c66ceec047d77c7fd616473c48426b9f
Lattecast <rattack.hyde@gmail.com>
2024-11-14 10:56:49 +0100

    Refactorización del DTO de pedidos y de detalles para implementar un builder

--- src/main/java/com/javalopers/tiendafacil/backend/dto/OrderDetailsDTO.java
+++ src/main/java/com/javalopers/tiendafacil/backend/dto/OrderDetailsDTO.java
@@ -1,9 +1,11 @@
 package com.javalopers.tiendafacil.backend.dto;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class OrderDetailsDTO {
 
     private Integer detailsId;
--- src/main/java/com/javalopers/tiendafacil/backend/service/OrderServiceImpl.java
+++ src/main/java/com/javalopers/tiendafacil/backend/service/OrderServiceImpl.java
@@ -19,13 +19,88 @@ import java.util.NoSuchElementException;
 
 @Service
 @AllArgsConstructor
-@Transactional
 public class OrderServiceImpl implements OrderService {
 
     private final OrderRepository orderRepository;
     private final OrderStatusRepository orderStatusRepository;
     private final ProductRepository productRepository;
     private final CustomerRepository customerRepository;
+
+
+
+    @Override
+    @Transactional
+    public OrderDTO saveOrder(OrderDTO orderDTO) {
+        Order order = convertToEntity(orderDTO);
+        order = orderRepository.save(order);
+        return convertToDTO(order);
+    }
+
+    @Override
+    @Transactional
+    public OrderDTO updateOrder(Integer orderId, OrderDTO orderDTO) {
+        Order order = orderRepository
+                .findById(orderId)
+                .orElseThrow(() -> new NoSuchElementException("No se encuentra el pedido con ID: " + orderId));
+
+        updateEntityWithDTO(orderDTO, order);
+        order = orderRepository.save(order);
+        return convertToDTO(order);
+    }
+
+    @Override
+    @Transactional
+    public OrderDTO findOrderById(Integer orderId) {
+        Order order = orderRepository
+                .findById(orderId)
+                .orElseThrow(() ->
+                        new NoSuchElementException("No se encuentra el pedido con ID: " + orderId)
+                );
+        return convertToDTO(order);
+    }
+
+
+    @Override
+    @Transactional
+    public void deleteOrderById(Integer orderId) {
+        if (!orderRepository.existsById(orderId)) {
+            throw new NoSuchElementException("Order not found with id: " + orderId);
+        }
+        orderRepository.deleteById(orderId);
+    }
+
+    @Override
+    @Transactional
+    public List<OrderDTO> findAllOrders() {
+        return orderRepository.findAll()
+                .stream()
+                .map(this::convertToDTO)
+                .toList();
+    }
+
+    private OrderDTO convertToDTO(Order entity) {
+        return OrderDTO.builder()
+                .orderId(entity.getOrderId())
+                .orderDate(entity.getOrderDate())
+                .deliveryDate(entity.getDeliveryDate())
+                .statusId(entity.getStatus().getStatusId())
+                .customerId(entity.getCustomer().getCustomerId())
+                .orderDetails(entity.getOrderDetails()
+                        .stream()
+                        .map(this::convertOrderDetailsToDTO)
+                        .toList())
+                .build();
+    }
+
+    private OrderDetailsDTO convertOrderDetailsToDTO(OrderDetails orderDetails) {
+        return OrderDetailsDTO.builder()
+                .orderId(orderDetails.getOrder().getOrderId())
+                .productId(orderDetails.getProduct().getProductId())
+                .detailsId(orderDetails.getDetailsId())
+                .productAmount(orderDetails.getProductAmount())
+                .build();
+    }
+
 
     private Order convertToEntity(OrderDTO dto) {
         Order order = new Order();
@@ -80,76 +155,6 @@ public class OrderServiceImpl implements OrderService {
                 .orElseThrow(() -> new NoSuchElementException(
                         "No se encuentra el cliente con ID: " + orderDTO.getCustomerId()
                 )));
-    }
-
-    @Override
-    @Transactional
-    public OrderDTO saveOrder(OrderDTO orderDTO) {
-        Order order = convertToEntity(orderDTO);
-        order = orderRepository.save(order);
-        return convertToDTO(order);
-    }
-
-    @Override
-    @Transactional
-    public OrderDTO updateOrder(Integer orderId, OrderDTO orderDTO) {
-        Order order = orderRepository
-                .findById(orderId)
-                .orElseThrow(() -> new NoSuchElementException("No se encuentra el pedido con ID: " + orderId));
-
-        updateEntityWithDTO(orderDTO, order);
-        order = orderRepository.save(order);
-        return convertToDTO(order);
-    }
-
-    @Override
-    public OrderDTO findOrderById(Integer orderId) {
-        Order order = orderRepository
-                .findById(orderId)
-                .orElseThrow(() ->
-                        new NoSuchElementException("No se encuentra el pedido con ID: " + orderId)
-                );
-        return convertToDTO(order);
-    }
-
-
-    @Override
-    public void deleteOrderById(Integer orderId) {
-        if (!orderRepository.existsById(orderId)) {
-            throw new NoSuchElementException("Order not found with id: " + orderId);
-        }
-        orderRepository.deleteById(orderId);
-    }
-
-    @Override
-    public List<OrderDTO> findAllOrders() {
-        return orderRepository.findAll()
-                .stream()
-                .map(this::convertToDTO)
-                .toList();
-    }
-
-    private OrderDTO convertToDTO(Order entity) {
-        OrderDTO dto = new OrderDTO();
-        dto.setOrderId(entity.getOrderId());
-        dto.setOrderDate(entity.getOrderDate());
-        dto.setDeliveryDate(entity.getDeliveryDate());
-        dto.setStatusId(entity.getStatus().getStatusId());
-        dto.setCustomerId(entity.getCustomer().getCustomerId());
-        dto.setOrderDetails(entity.getOrderDetails()
-                .stream()
-                .map(this::convertOrderDetailsToDTO)
-                .toList());
-        return dto;
-    }
-
-    private OrderDetailsDTO convertOrderDetailsToDTO(OrderDetails orderDetails) {
-        OrderDetailsDTO dto = new OrderDetailsDTO();
-        dto.setOrderId(orderDetails.getOrder().getOrderId());
-        dto.setDetailsId(orderDetails.getDetailsId());
-        dto.setProductId(orderDetails.getProduct().getProductId());
-        dto.setProductAmount(orderDetails.getProductAmount());
-        return dto;
     }
 
 }
